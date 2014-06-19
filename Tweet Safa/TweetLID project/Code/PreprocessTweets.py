dfbf63b8099e2044fc50a9a59f27336e969f58a2
imendibo <iosu.mendizabal@gmail.com>
2014-06-19 15:32:06 +0200

    A

--- Tweet Safa/TweetLID project/Code/PreprocessTweets.py
+++ Tweet Safa/TweetLID project/Code/PreprocessTweets.py
@@ -1,11 +1,27 @@
 import ReadData as read
 import re
+import string
 
 def lower_case(tweet):
     return tweet.decode('utf-8').lower()
 
 def remove_multiple_spaces(tweet):
-    return re.sub(' +',' ',tweet)
+    tweet = ' ' + tweet + ' '
+    tweet = re.sub(' +',' ',tweet)
+    return tweet
+
+def remove_puntuation(tweet):
+    return reduce(lambda tweet, c: tweet.replace(c, ' '), string.punctuation, tweet)
+
+def remove_url(tweet):
+    #p = re.compile('/((?:https?\:\/\/|www\.)(?:[-a-z0-9]+\.)*[-a-z0-9]+.*)/i')
+
+    #a = re.compile('/(pic.twitter+.# *)/i')
+    #tweet = re.sub(a,'',tweet)
+
+    return re.sub(r'^https?:\/\/.*[\r\n]*', '', tweet, flags=re.MULTILINE)
+
+    #return re.sub(p,'', tweet)
 
 def main(tweetList):
     tweetListPreprocessed = []
@@ -13,6 +29,8 @@ def main(tweetList):
 
     for tweet in tweetList:
         tweetPreprocessed = lower_case(tweet.text)
+        tweetPreprocessed = remove_url(tweetPreprocessed)
+        tweetPreprocessed = remove_puntuation(tweetPreprocessed)
         tweetPreprocessed = remove_multiple_spaces(tweetPreprocessed)
 
         # Save in new object
