020e89f5688708a039600d797b256ca7e8abf2e1
imendibo <iosu.mendizabal@gmail.com>
2014-06-19 16:56:15 +0200

    a

--- Tweet Safa/TweetLID project/Code/PreprocessTweets.py
+++ Tweet Safa/TweetLID project/Code/PreprocessTweets.py
@@ -19,22 +19,13 @@ def format_puntuation(tweet):
     return reduce(lambda tweet, c: tweet.replace(c, ' ' + c + ' '), punctuation, tweet)
 
 def remove_url(tweet):
-
-    URLless_string = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', tweet)
-
-    return URLless_string
+    return re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', tweet)
 
 def remove_usernames(tweet):
-
-    usernameLess_string = re.sub('@[^\s]+', '', tweet)
-
-    return usernameLess_string
+    return re.sub('@[^\s]+', '', tweet)
 
 def remove_pic_twitter(tweet):
-
-    result = re.sub('pic.twitter[^\s|,]+', '', tweet)
-
-    return result
+    return re.sub('pic.twitter[^\s|,]+', '', tweet)
 
 def main(tweetList):
     tweetListPreprocessed = []
