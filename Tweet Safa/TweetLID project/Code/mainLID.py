c80082f99262806b346882f445941e82d325155d
imendibo <iosu.mendizabal@gmail.com>
2014-06-19 12:36:07 +0200

    main and read data

--- Tweet Safa/TweetLID project/Code/mainLID.py
+++ Tweet Safa/TweetLID project/Code/mainLID.py
@@ -1,11 +1,12 @@
 
-
+import ReadData as read
 
 
 # Read dataset
-
-
+dataset = "../Dataset/output.txt"
+tweetList = read.read_tweets_dataset(dataset)
+print tweetList.__len__()
 # Pre-process state
 
 
-# Clean data -> 
+# Clean data ->
