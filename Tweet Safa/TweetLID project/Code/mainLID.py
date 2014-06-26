13c43acee21dacd4c29677e432b2bbe361416c31
imendibo <iosu.mendizabal@gmail.com>
2014-06-26 18:59:33 +0200

    as

--- Tweet Safa/TweetLID project/Code/mainLID.py
+++ Tweet Safa/TweetLID project/Code/mainLID.py
@@ -2,6 +2,7 @@
 from __future__ import division
 import ReadData as read
 import PreprocessTweets as preprocess
+import CrossValidation as cv
 import UtilsTweetSafa as utils
 import Smoothing as linear
 import CrossValidation as cv
@@ -11,14 +12,14 @@ import sys
 maxNgram=5
 # 1-. Read dataset and create tweetList fullfilled of Tweet object*
 
-dataset = "../Dataset/output_complete.txt"
+dataset = "../Dataset/output.txt"
 
 tweetList = read.read_tweets_dataset(dataset)
 
 # 2-. Pre-process state
 
 tweetListPreProcessed = preprocess.main(tweetList)
-corpusNgrams, arrayLanguages,arrayLanguagesFull=utils.obtainNgrams(tweetListPreProcessed, maxNgram)
+corpusNgrams, arrayLanguages,arrayLanguagesFull=utils.obtainNgrams(tweetList,maxNgram)
 arrayLanguagesFull = utils.orderVector(arrayLanguagesFull)
     # Raw data -> tweetList
     # Clean data -> tweetListPreProcessed
@@ -28,8 +29,9 @@ arrayLanguagesFull = utils.orderVector(arrayLanguagesFull)
 # 3-. Algorithms
 #
 # 3.1-. OBTAIN N-GRAMS
-
-
+#
+# corpusNgrams, arrayLanguages = utils.obtainNgrams(tweetListPreProcessed)
+#
 
 #print(corpusNgrams.get(str(4)).get('es'))
 
@@ -37,9 +39,54 @@ arrayLanguagesFull = utils.orderVector(arrayLanguagesFull)
 
 # Clean data -> Algorithm
 
+# 3.2-. Linear interpolation
+#   Generate linear coefficients: input (n-grams and language)
+#   Smooth data
 
-
-prob = 1.0;
+# linearCoefficients = list()
+#
+# for language in arrayLanguages:
+#     unigrams = corpusNgrams.get('1').get(language)
+#     bigrams = corpusNgrams.get('2').get(language)
+#     trigrams = corpusNgrams.get('3').get(language)
+#     linearCoefficients.append(linear.getlinearcoefficients(language, unigrams, bigrams, trigrams))
+#
+# max = 0;
+# tweetEN = "Tomorrow is going to be a good day to go to the beach"
+# tweetPT = "Amanhã será um dia muito bom, como ir para a praia."
+# tweetCA = "Demà farà un dia molt bo, com per anar a la platja."
+# tweetEU = "Bihar egun oso ona egingo du, hondartzara joateko modukoa."
+# tweetGL = "Mañá será un día moi bo, como ir á praia."
+# tweetES = "Mañana hará un dia muy bueno, como para ir a la playa."
+#
+# text = preprocess.preprocessText(tweetES)
+#
+# print text
+#
+# for linearCoefficients in linearCoefficients:
+#     # print str(linearCoefficients[0])+" "+str(linearCoefficients[1])+" "+str(linearCoefficients[2]) + " " + str(linearCoefficients[3])+"\n"
+#
+#     prob = 1.0;
+#     for i in range(0,len(text)-3):
+#         x = text[i]; y = text[i+1]; z = text[i+2]
+#         probability = linear.probability(corpusNgrams, linearCoefficients, x, y, z)
+#         # print probability
+#         prob = prob * probability
+#
+#     if prob >= max:
+#         language = linearCoefficients[0]
+#         max = prob
+#
+#
+#     sys.stdout.write("Sequence probability in "+str(linearCoefficients[0])+": "+str(prob)+"\n")
+#
+#
+# sys.stdout.write("\n    Tweet:  "+str(text.encode("utf-8")))
+# sys.stdout.write("\n    Tweet language:   "+str(language)+"\n    Probability of:  "+str(max)+"\n")
+#
+#
+#
+# prob = 1.0;
 # for i in range(0,len(text)-3):
 #     x = text[i]; y = text[i+1]; z = text[i+2]
 #     probability = linear.probability(corpusNgrams, linearCoefficients, x, y, z)
@@ -49,55 +96,8 @@ prob = 1.0;
 
 
 # 3.2-. Algorithms: Bayesian Networks
-#   3.2.1-. Linear interpolation
-#       Generate linear coefficients: input (n-grams and language)
-#       Smooth data
-
-linearCoefficients = list()
-
-for language in arrayLanguages:
-    unigrams = corpusNgrams.get('1').get(language)
-    bigrams = corpusNgrams.get('2').get(language)
-    trigrams = corpusNgrams.get('3').get(language)
-
-    linearCoefficients.append(linear.getlinearcoefficients(language, unigrams, bigrams, trigrams))
-
-max = 0;
-tweetEN = "Tomorrow is going to be a good day to go to the beach"
-tweetPT = "Amanhã será um dia muito bom, como ir para a praia."
-tweetCA = "Demà farà un dia molt bo, com per anar a la platja."
-tweetEU = "Bihar egun oso ona egingo du, hondartzara joateko modukoa."
-tweetGL = "Mañá será un día moi bo, como ir á praia."
-tweetES = "Mañana hará un dia muy bueno, como para ir a la playa."
-
-text = preprocess.preprocessText(tweetES)
-
-print text
-
-for linearCoefficients in linearCoefficients:
-    # print str(linearCoefficients[0])+" "+str(linearCoefficients[1])+" "+str(linearCoefficients[2]) + " " + str(linearCoefficients[3])+"\n"
-
-    prob = 1.0;
-    for i in range(0,len(text)-3):
-        x = text[i]; y = text[i+1]; z = text[i+2]
-        probability = linear.probability(corpusNgrams, linearCoefficients, x, y, z)
-        # print probability
-        prob = prob * probability
-
-    if prob >= max:
-        language = linearCoefficients[0]
-        max = prob
-
-
-    sys.stdout.write("Sequence probability in "+str(linearCoefficients[0])+": "+str(prob)+"\n")
-
-
-sys.stdout.write("\n    Tweet:  "+str(text.encode("utf-8")))
-sys.stdout.write("\n    Tweet language:   "+str(language)+"\n    Probability of:  "+str(max)+"\n")
 
 # 3.3-. Algorithms: Ranking Methods
-
 print(arrayLanguagesFull)
 cv.nestedCrossValidation(tweetListPreProcessed,5,5,[0,0,0],arrayLanguagesFull)
 # 3.4-. Out-of-place Measure
-
