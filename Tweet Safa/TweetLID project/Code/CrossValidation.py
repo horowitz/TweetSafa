338e0e2db5a4bbc06faf2996ba0c25eb364eb590
Jeroni Carandell Saldich <jeronicarandell@gmail.com>
2014-06-26 13:25:14 +0200

    Complete Utils for Ranking

--- Tweet Safa/TweetLID project/Code/CrossValidation.py
+++ Tweet Safa/TweetLID project/Code/CrossValidation.py
@@ -1,32 +1,23 @@
 from __future__ import division
+import nltk as nk
 __author__ = 'danielhorowitz'
 import math
-import numpy as np
 import UtilsTweetSafa as utils
 def nestedCrossValidation(tweetList, k, c,models):
-    # dataset = np.array(tweetList)
+    m,n=80,50
     for i in xrange(k-1):
         trainAndValidationSet,testSet = divideDataset(tweetList,k,i)
         for j in xrange(c-1):
             trainSet,validationSet = divideDataset(trainAndValidationSet,c,j)
-            confidenceDict=utils.learnNgramConfidencefromData(trainSet,validationSet)
-            for tweet in validationSet:
+            trainDist = utils.obtainNgrams(trainSet,6)
+            validationSetFreq=utils.obtainNgrams(validationSet,6)
+            confidenceDict=utils.learnNgramConfidencefromData(trainDist,validationSet)
+            predicted,true=utils.evaluateNgramRakingSet(validationSet,trainDist, confidenceDict,m,n)
+            print(nk.ConfusionMatrix(predicted,true))
 
-
-# def divideDataset(dataset,k,index):
-#     mask = np.ones(len(dataset), dtype=bool)
-#     indexes = range(int(math.ceil(len(dataset)*index/k)),int(math.ceil(len(dataset)*(index+1)/k)))
-#     mask[indexes] = False
-#     testSet = dataset[indexes]
-#     trainSet = dataset[mask]
-#     return (trainSet,testSet)
 
 def divideDataset(dataset,k,index):
     testSet = dataset[int(math.ceil(len(dataset)*index/k)):int(math.ceil(len(dataset)*(index+1)/k))]
     trainSet = dataset[0:int(math.ceil(len(dataset)*index/k))] + dataset[int(math.ceil(len(dataset)*(index+1)/k)):len(dataset)]
     return (trainSet,testSet)
-# # Divide the dataset into k chunks
-# def chunks(l, k):
-#     n = int(math.ceil(len(l)/k))
-#     return [l[i:i + n] for i in range(0, len(l), n)]
 
--- Tweet Safa/TweetLID project/Code/UtilsTweetSafa.py
+++ Tweet Safa/TweetLID project/Code/UtilsTweetSafa.py
@@ -4,11 +4,9 @@ import nltk as nk
 import PreprocessTweets as preprocess
 
 # This method concatenates tweets
-
 def concatenateLanguageTweets(List):
     corpus = dict()
     languageArray = list()
-
     for tweet in List:
         if ~corpus.has_key(tweet.language) & (corpus.get(tweet.language) is None):
             corpus[tweet.language] = tweet.text
@@ -18,11 +16,9 @@ def concatenateLanguageTweets(List):
     return corpus, languageArray
 
 # Separate by individual languages(en,es,eu,ca,gl,pt,und,other). Return a dictionary of individual languages
-
 def separateIndividualLanguages(List):
     individualCorpus = dict()
     languageArray = list()
-
     for key in List.keys():
         if (not '+' in key) and (not '/' in key):
             individualCorpus[key] = List.get(key)
@@ -30,7 +26,6 @@ def separateIndividualLanguages(List):
             for subKey in List.keys():
                 if key in subKey and not key is subKey:
                     individualCorpus[key] = individualCorpus[key] + List.get(subKey)
-
     return individualCorpus, languageArray
 
 # N-gram Frequency distributions for all N and for all Languages.
@@ -39,19 +34,14 @@ def separateIndividualLanguages(List):
 
 def freqDistributions(corpus, maxNgram):
     corpusNgrams = dict()
-
     for N in xrange(1, maxNgram):
         auxCorpus = dict()
-
         for key in corpus.keys():
             auxCorpus[key] = getFreqDist(corpus.get(key), N)
-
         corpusNgrams[str(N)] = auxCorpus
-
     return corpusNgrams
 
 # returns N-gram distribution given a text
-
 def getFreqDist(text, n):
     ngramsObject = nk.ngrams(text, n)
     freqDist = nk.FreqDist(ngramsObject)
@@ -68,41 +58,28 @@ def printTweets(tweetList):
 def obtainNgrams(tweetListPreProcessed,maxNgram):
     # Join all the tweets in one language. Return one dictionary of languages
     corpus, arrayLanguages = concatenateLanguageTweets(tweetListPreProcessed)
-
     # individualLanguage=true:
     #       Only individual languages(en,es,..)
-
     # individualLanguage=false:
     #       Mixed languages(en+es,pt+gl,..)
-
     individualLanguage = True
-
     if individualLanguage:
         corpus, arrayLanguages = separateIndividualLanguages(corpus)
-
     # clean dictionary of double spaces from concatenation
     for key in corpus.keys():
         corpus[key] = preprocess.remove_multiple_spaces(corpus.get(key))
-
-
-
     corpusNgrams = freqDistributions(corpus, maxNgram)
-
     return corpusNgrams, arrayLanguages
 
 # Calculates out of place measure
 def outofplaceMeasure(FDLenght, TTLenght, freqDist, freqDistTest):
-    outofplaceResult = list()
-
     FDLenght = min(len(freqDist), FDLenght)
     TTLenght = min(len(freqDistTest), TTLenght)
     # Get m x n items
     topFDItems = freqDist.items()[:FDLenght]
     topTTItems = freqDistTest.items()[:TTLenght]
-
     totalDistance = 0
     for i in xrange(0, TTLenght):
-        # print(testText + "\t" + str(TTLenght) + "\t" +str(len(topTTItems)))
         lp = topTTItems[i]
         distance = FDLenght
         for j in xrange(0, FDLenght):
@@ -118,32 +95,76 @@ def learnNgramConfidences(confidenceDict,corpusNgrams,tweet,m,n):
     acc=0
     tot=0
     label=tweet.language
-    ngramPredictedLanguage=list()
     for key in corpusNgrams.keys():
         predictedLanguage=list()
-        languagesList= None
-        print('N: '+ key)
         languagesList=corpusNgrams.get(key).keys()
         for subkey in languagesList:
-            # print ('Length' + str(len(corpusNgrams.get(key).get(subkey)) ))
             predictedLanguage.append(outofplaceMeasure(m,n,corpusNgrams.get(key).get(subkey),getFreqDist(tweet.text,int(float(key)))))
         predicted=languagesList[predictedLanguage.index(min(predictedLanguage))]
         if label == predicted:
             confidenceDict[key]=confidenceDict[key]+1
             acc=acc+1
         tot=tot+1
-        print('True: '+label+' Predicted: '+predicted)
-    print(str(acc/tot))
     return confidenceDict,tot
 
 # returns confidence of each N-gram to be a good guesser for a whole train set.
-def learnNgramConfidencefromData(trainSet,validationSet):
-    trainDist = obtainNgrams(trainSet,5)
+def learnNgramConfidencefromData(trainDist,validationSet):
     confidenceDict=dict((el,0) for el in trainDist[0].keys())
     tot=0
     for tweet in validationSet:
         confidenceDict , totAux = learnNgramConfidences(confidenceDict,trainDist[0],tweet,80,50)
-        print (confidenceDict)
         tot=tot+totAux
     confidenceDict = dict((el,confidenceDict.get(el)/tot) for el in confidenceDict.keys())
-    print(confidenceDict)
+    return confidenceDict
+
+# evaluate test Set
+def evaluateNgramRakingSet(validationSet, trainFreq,confidenceNgrams,m,n):
+    predictedLanguage=list()
+    trueLanguage=list()
+    for tweet in validationSet:
+        trueLanguage.append(tweet.language)
+        predictedLanguage.append(evaluateNgramRanking(tweet, trainFreq,confidenceNgrams,m,n))
+    return predictedLanguage, trueLanguage
+
+# evaluate single tweet
+def evaluateNgramRanking(tweet,trainFreq,confidenceDict,m,n):
+    acc=0
+    tot=0
+    label=tweet.language
+    predictedDict=dict()
+    for key in trainFreq[0].keys():
+        predictedLanguage=list()
+        languagesList=trainFreq[0].get(key).keys()
+        for subkey in languagesList:
+            predictedLanguage.append(outofplaceMeasure(m,n,trainFreq[0].get(key).get(subkey),getFreqDist(tweet.text,int(float(key)))))
+        predicted=languagesList[predictedLanguage.index(min(predictedLanguage))]
+        if label == predicted:
+            acc=acc+1
+        tot=tot+1
+        if not predictedDict.has_key(predicted):
+            predictedDict[predicted] = confidenceDict.get(key)
+        else:
+            predictedDict[predicted] = predictedDict.get(predicted) + confidenceDict.get(key)
+    predictedL=chooseLanguages(predictedDict,0.01)
+    return predictedL
+
+# choose best languages
+def chooseLanguages(predictedDict,threshold):
+    items = [(v, k) for k, v in predictedDict.items()]
+    items.sort()
+    items.reverse()
+    items = [(k, v) for v, k in items]
+    language,value = items.pop(0)
+    count = 0
+    if not language=='other' and not language=='und':
+        for k,v in items:
+            count += 1
+            if count == 1:
+                continue
+            else:
+                if value-v < threshold and not count > 2 :
+                    if not k=='other' and not k=='und':
+                        language = language+'+'+k
+                else:
+                    break
+    return language
