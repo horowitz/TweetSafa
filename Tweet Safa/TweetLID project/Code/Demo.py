fbe7a0376c5ce2c254372666c9f30b6ce8860348
imendibo <iosu.mendizabal@gmail.com>
2014-07-01 15:32:35 +0200

    Bash file for evaluation

--- Tweet Safa/TweetLID project/Code/Demo.py
+++ Tweet Safa/TweetLID project/Code/Demo.py
@@ -58,8 +58,8 @@ realES = "#Lugo #a6 (amarillo) obras en #PedrafitaDoCebreiro carril izquierdo ce
 realUND = "Hhhhhhhhhhhhjjhhhhhhhhhhhh"
 realOTHER = "Buongiorno ai nostri ascoltatori in Italia :)) pic.twitter.com/zfGpYc3oxo"
 
-a = 'Primer sorteo del stream @Dimegioclub http://www.twitch.tv/miicrocs'
-text = preprocess.preprocessText(realGL)
+a = 'Primer sorteo del stream @Dimegioclub http://www.twitch.tv/miicrocs' #en+es
+text = preprocess.preprocessText(a)
 
 linearCoefficients = linear.getlinearcoefficientsForLanguageArray(arrayLanguages, maxNgram, corpusNgrams)
 predictedLanguage, probability = linear.getPredictedLanguageForTweet(linearCoefficients, text, maxNgram, corpusNgrams)
